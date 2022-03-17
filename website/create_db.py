6018d466317c88216ad2d2f63e7b87f5c0e51867
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2022-03-16 17:53:45 -0700

    Update create_db.py

--- website/create_db.py
+++ website/create_db.py
@@ -5,14 +5,14 @@ import mysql.connector
 # Phrase 2 wants username="comp440" and password="pass1234"
 mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
+    user="comp440",
     passwd="pass1234"
 )
 
 my_cursor = mydb.cursor()
 
 # Already created now
-#my_cursor.execute("CREATE DATABASE user_database")
+my_cursor.execute("CREATE DATABASE user_database")
 #my_cursor.execute("DROP DATABASE user_database")
 
 my_cursor.execute("SHOW DATABASES")
