5f979c4602fd8b0bba439ca478462cb3b5b8e45f
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2022-04-02 17:41:10 -0700

    Comments method implemented

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 from sympy import re
@@ -150,3 +151,70 @@ def posts(username):
 
 # ----------------------------------- Comments Mehtods -------------------------------------------------------
 
+# Create comments on other posts
+@views.route("/create-comment/<post_id>", methods=['GET', 'POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+    sentiment = request.form.get('sentiment')
+    cursor = mydb.cursor()
+
+    # Query to count the comment made on that post from the user
+    query = "SELECT COUNT(comment.id) FROM comment INNER JOIN post ON comment.post_id = post.id WHERE post.id = (%s) AND comment.author = (%s)"
+    cursor.execute(query, (post_id, current_user.id))
+    result = cursor.fetchone()[0] + 1
+
+    # Query to count the number of comments made by the user on the current date
+    query2 = "SELECT COUNT(*) FROM comment WHERE comment.dateCreatedOn = CURRENT_DATE AND comment.author = %s"
+    cursor.execute(query2, [current_user.id])
+    commented_today = cursor.fetchone()[0] + 1
+    
+    # Query to select DISTINCT author relating to the post.id
+    query3 = "SELECT DISTINCT post.author FROM post WHERE post.id = %s"
+    cursor.execute(query3, [post_id])
+    current_author = cursor.fetchone()[0]
+    mydb.commit()
+    cursor.close()
+
+    print(current_user.id)
+    print(result)
+
+    # Checks to make sure the user is not commenting on their own posts, 
+    # commenting on the same post twice, or exceeds the max limit per day
+    if current_user.id == current_author:
+        flash('Can not comment on your own post!', category='error')
+    elif result > 1:
+        flash('Can not comment more than once on the same post!', category='error')
+    elif commented_today > 3:
+        flash('Can not comment more than 3 times per day!', category='error')
+    else:
+        if not text:
+            flash('Comment input cannot be empty!', category='error')
+        else:
+            # If everything is okay, add comment to table corresponding to the post
+            post = Post.query.filter_by(id = post_id)
+
+            if post:
+                comment = Comment(text=text, sentiment=sentiment, author=current_user.id, post_id=post_id)
+                db.session.add(comment)
+                db.session.commit()
+            else:
+                flash('Post does not exist', category='error')
+
+    return redirect(url_for('views.post_main'))
+
+# Delete comment made on a post
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exists.', category='error')
+    elif (current_user.id != comment.author) and (current_user.id != comment.post.author):
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+    
+    return redirect(url_for('views.post_main'))
